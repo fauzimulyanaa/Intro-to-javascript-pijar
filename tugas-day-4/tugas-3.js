9129ccb78c45bf515f8b6fdeba9ff2e1c6c406ca
Fauzi Mulyana <94151536+fauzimulyanaa@users.noreply.github.com>
2023-09-18 15:37:36 +0700

    Update tugas-3.js

--- tugas-day-4/tugas-3.js
+++ tugas-day-4/tugas-3.js
@@ -12,14 +12,16 @@ console.log(hasil);
 
 const returnDay = (clock) => {
   for (let i = 0; i <= clock; i++) {
-    if (clock >= 5 && clock <= 12) {
+       if (clock >= 5 && clock <= 12) {
       return "Pagi";
     } else if (clock >= 13 && clock <= 15) {
       return "Siang";
     } else if (clock >= 16 && clock <= 18) {
       return "Sore";
-    } else {
+    } else if ((clock >= 19 && clock <= 24) || (clock >= 0 && clock <= 4)) {
       return "Malam";
+    } else {
+      return "Masukan Waktu Yang Benar!";
     }
   }
 };
