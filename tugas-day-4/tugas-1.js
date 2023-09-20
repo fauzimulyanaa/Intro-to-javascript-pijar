b7801e14ca0b60b887aeab8bb22210ab8ddef648
Fauzi Mulyana <94151536+fauzimulyanaa@users.noreply.github.com>
2023-09-20 13:29:16 +0700

    Update tugas-1.js

--- tugas-day-4/tugas-1.js
+++ tugas-day-4/tugas-1.js
@@ -1,14 +1,9 @@
 let biodata = {};
 
 const storedObjects = (key, value) => {
-  const newObj = { ...biodata, [key]: value };
-
-  return newObj;
+  biodata = { ...biodata, [key]: value };
 };
-
-biodata = storedObjects("nama", "Fauzi");
-
-biodata = storedObjects("alamat", "Cicaheum");
-biodata = storedObjects("age", 23);
+storedObjects("nama", "Fauzi");
+storedObjects("age", "23");
 
 console.log(biodata);
